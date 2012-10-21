bfee77c5be61317c9c84d80268256a533fce78fa
Адонай Элохим <adonai@xaker.ru>
2012-10-22 02:24:53 +1100

    Окончательный вариант размещения постов

--- src/adonai/diary_browser/MessageSender.java
+++ src/adonai/diary_browser/MessageSender.java
@@ -32,6 +32,18 @@ public class MessageSender extends Activity implements OnClickListener, OnChecke
 	CheckBox mShowOptionals;
 	CheckBox mShowPoll;
 	
+	TextView mPollTitle;
+	TextView mPollChoice1;
+	TextView mPollChoice2;
+	TextView mPollChoice3;
+	TextView mPollChoice4;
+	TextView mPollChoice5;
+	TextView mPollChoice6;
+	TextView mPollChoice7;
+	TextView mPollChoice8;
+	TextView mPollChoice9;
+	TextView mPollChoice10;
+	
 	List<View> optionals = new ArrayList<View>();
 	List<View> pollScheme = new ArrayList<View>();
 	
@@ -62,9 +74,22 @@ public class MessageSender extends Activity implements OnClickListener, OnChecke
     	mPublish = (Button)findViewById(R.id.message_publish);
     	mPublish.setOnClickListener(this);
     	
+    	mPollTitle = (TextView) findViewById(R.id.message_poll_title);
+    	mPollChoice1 = (TextView) findViewById(R.id.message_poll_1);
+    	mPollChoice2 = (TextView) findViewById(R.id.message_poll_2);
+    	mPollChoice3 = (TextView) findViewById(R.id.message_poll_3);
+    	mPollChoice4 = (TextView) findViewById(R.id.message_poll_4);
+    	mPollChoice5 = (TextView) findViewById(R.id.message_poll_5);
+    	mPollChoice6 = (TextView) findViewById(R.id.message_poll_6);
+    	mPollChoice7 = (TextView) findViewById(R.id.message_poll_7);
+    	mPollChoice8 = (TextView) findViewById(R.id.message_poll_8);
+    	mPollChoice9 = (TextView) findViewById(R.id.message_poll_9);
+    	mPollChoice10 = (TextView) findViewById(R.id.message_poll_10);
+    	
     	mShowOptionals = (CheckBox) findViewById(R.id.message_optional);
     	mShowOptionals.setOnCheckedChangeListener(this);
     	mShowPoll = (CheckBox) findViewById(R.id.message_poll);
+    	mShowPoll.setOnCheckedChangeListener(this);
     	
     	optionals.add(findViewById(R.id.message_themes_hint));
     	optionals.add(themesText);
@@ -72,6 +97,20 @@ public class MessageSender extends Activity implements OnClickListener, OnChecke
     	optionals.add(musicText);
     	optionals.add(findViewById(R.id.message_mood_hint));
     	optionals.add(moodText);
+    	
+    	pollScheme.add(mPollTitle);
+    	pollScheme.add(mPollChoice1);
+    	pollScheme.add(mPollChoice2);
+    	pollScheme.add(mPollChoice3);
+    	pollScheme.add(mPollChoice4);
+    	pollScheme.add(mPollChoice5);
+    	pollScheme.add(mPollChoice6);
+    	pollScheme.add(mPollChoice7);
+    	pollScheme.add(mPollChoice8);
+    	pollScheme.add(mPollChoice9);
+    	pollScheme.add(mPollChoice10);
+    	
+    	
     }
 
     /* (non-Javadoc)
@@ -119,24 +158,51 @@ public class MessageSender extends Activity implements OnClickListener, OnChecke
 					
 					postParams.add(new BasicNameValuePair("title", mPost.get_title()));
 					postParams.add(new BasicNameValuePair("message", mPost.get_text().toString()));
-					postParams.add(new BasicNameValuePair("themes", mPost.get_themes()));
-					postParams.add(new BasicNameValuePair("current_music", mPost.get_music()));
-					postParams.add(new BasicNameValuePair("current_mood", mPost.get_mood()));
+					
+					if(mShowOptionals.isChecked())
+					{
+						postParams.add(new BasicNameValuePair("themes", mPost.get_themes()));
+						postParams.add(new BasicNameValuePair("current_music", mPost.get_music()));
+						postParams.add(new BasicNameValuePair("current_mood", mPost.get_mood()));
+					}
+					else
+					{
+						postParams.add(new BasicNameValuePair("themes", ""));
+						postParams.add(new BasicNameValuePair("current_music", ""));
+						postParams.add(new BasicNameValuePair("current_mood", ""));
+					}
 					
 					postParams.add(new BasicNameValuePair("attachment", ""));
 					postParams.add(new BasicNameValuePair("close_text", ""));
 					
-					postParams.add(new BasicNameValuePair("poll_title", ""));
-					postParams.add(new BasicNameValuePair("poll_answer_1", ""));
-					postParams.add(new BasicNameValuePair("poll_answer_2", ""));
-					postParams.add(new BasicNameValuePair("poll_answer_3", ""));
-					postParams.add(new BasicNameValuePair("poll_answer_4", ""));
-					postParams.add(new BasicNameValuePair("poll_answer_5", ""));
-					postParams.add(new BasicNameValuePair("poll_answer_6", ""));
-					postParams.add(new BasicNameValuePair("poll_answer_7", ""));
-					postParams.add(new BasicNameValuePair("poll_answer_8", ""));
-					postParams.add(new BasicNameValuePair("poll_answer_9", ""));
-					postParams.add(new BasicNameValuePair("poll_answer_10", ""));
+					if(mShowPoll.isChecked())
+					{
+						postParams.add(new BasicNameValuePair("poll_title", mPollTitle.getText().toString()));
+						postParams.add(new BasicNameValuePair("poll_answer_1", mPollChoice1.getText().toString()));
+						postParams.add(new BasicNameValuePair("poll_answer_2", mPollChoice2.getText().toString()));
+						postParams.add(new BasicNameValuePair("poll_answer_3", mPollChoice3.getText().toString()));
+						postParams.add(new BasicNameValuePair("poll_answer_4", mPollChoice4.getText().toString()));
+						postParams.add(new BasicNameValuePair("poll_answer_5", mPollChoice5.getText().toString()));
+						postParams.add(new BasicNameValuePair("poll_answer_6", mPollChoice6.getText().toString()));
+						postParams.add(new BasicNameValuePair("poll_answer_7", mPollChoice7.getText().toString()));
+						postParams.add(new BasicNameValuePair("poll_answer_8", mPollChoice8.getText().toString()));
+						postParams.add(new BasicNameValuePair("poll_answer_9", mPollChoice9.getText().toString()));
+						postParams.add(new BasicNameValuePair("poll_answer_10", mPollChoice10.getText().toString()));
+					}
+					else
+					{
+						postParams.add(new BasicNameValuePair("poll_title", ""));
+						postParams.add(new BasicNameValuePair("poll_answer_1", ""));
+						postParams.add(new BasicNameValuePair("poll_answer_2", ""));
+						postParams.add(new BasicNameValuePair("poll_answer_3", ""));
+						postParams.add(new BasicNameValuePair("poll_answer_4", ""));
+						postParams.add(new BasicNameValuePair("poll_answer_5", ""));
+						postParams.add(new BasicNameValuePair("poll_answer_6", ""));
+						postParams.add(new BasicNameValuePair("poll_answer_7", ""));
+						postParams.add(new BasicNameValuePair("poll_answer_8", ""));
+						postParams.add(new BasicNameValuePair("poll_answer_9", ""));
+						postParams.add(new BasicNameValuePair("poll_answer_10", ""));
+					}
 					
 					postParams.add(new BasicNameValuePair("rewrite", "rewrite"));
 					postParams.add(new BasicNameValuePair("save_type", "js2"));
@@ -163,8 +229,27 @@ public class MessageSender extends Activity implements OnClickListener, OnChecke
 
 	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) 
 	{
-
-		
+		switch(buttonView.getId())
+		{
+			case R.id.message_optional:
+				if(isChecked)
+					for(View view : optionals)
+						view.setVisibility(View.VISIBLE);
+				else
+					for(View view : optionals)
+						view.setVisibility(View.GONE);
+			break;
+			case R.id.message_poll:
+				if(isChecked)
+					for(View view : pollScheme)
+						view.setVisibility(View.VISIBLE);
+				else
+					for(View view : pollScheme)
+						view.setVisibility(View.GONE);
+			break;
+			default:
+			break;
+		}
 	}
     
 }
