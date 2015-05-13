fad7886dc8e2520f05b3511061e10fca538a2ac6
Адонай Элохим <adonai@xaker.ru>
2015-05-14 01:53:17 +0300

    hack fucking Cloud Flare

--- src/main/java/adonai/diary_browser/DiaryHttpClient.java
+++ src/main/java/adonai/diary_browser/DiaryHttpClient.java
@@ -5,6 +5,7 @@ import android.webkit.CookieManager;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.AbortableHttpRequest;
@@ -13,14 +14,20 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.params.ClientPNames;
 import org.apache.http.client.params.CookiePolicy;
 import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -30,11 +37,19 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.net.ssl.SSLException;
 
 public class DiaryHttpClient {
+    private final static Pattern OPERATION_PATTERN = Pattern.compile("setTimeout\\(function\\(\\)\\{\\s+(var t,r,a,f.+?\\r?\\n[\\s\\S]+?a\\.value =.+?)\\r?\\n");
+    private final static Pattern PASS_PATTERN = Pattern.compile("name=\"pass\" value=\"(.+?)\"");
+    private final static Pattern CHALLENGE_PATTERN = Pattern.compile("name=\"jschl_vc\" value=\"(\\w+)\"");
+    
     private URI currentUrl = URI.create("");
 
     HttpClient httpClient = new DefaultHttpClient();
@@ -43,8 +58,8 @@ public class DiaryHttpClient {
     List<AbortableHttpRequest> runningRequests = new ArrayList<>();
 
     public DiaryHttpClient() {
-        httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.BEST_MATCH);
-        httpClient.getParams().setParameter(CoreProtocolPNames.USER_AGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36");
+        httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.BROWSER_COMPATIBILITY);
+        httpClient.getParams().setParameter(CoreProtocolPNames.USER_AGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36");
         localContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
     }
 
@@ -62,6 +77,14 @@ public class DiaryHttpClient {
 
     public CookieStore getCookieStore() {
         return cookieStore;
+    }
+    
+    public boolean hasCookie(String name) {
+        for(Cookie cookie : cookieStore.getCookies()) {
+            if(cookie.getName().equals(name))
+                return true;
+        }
+        return false;
     }
 
     public String postPageToString(String url, HttpEntity data) throws IOException {
@@ -136,16 +159,95 @@ public class DiaryHttpClient {
      */
     public HttpResponse getPage(URI url) throws IOException {
         try {
-            DefaultHttpClient asyncRetriever = new DefaultHttpClient();
             HttpGet httpGet = new HttpGet(url);
             runningRequests.add(httpGet);
     
-            HttpResponse result = asyncRetriever.execute(httpGet, localContext);
+            HttpResponse result = httpClient.execute(httpGet, localContext);
             syncCookiesWithWebViews();
             return result;
         } catch (SSLException e) {
             return null;
         }
+    }
+
+    /**
+     * Manual page processing
+     * Remember to disconnect connection!
+     *
+     * @param url url to fetch
+     * @return connection for manual usage
+     */
+    public HttpResponse getPage(URI url, HashMap<String, String> headers) throws IOException {
+        try {
+            HttpGet httpGet = new HttpGet(url);
+            for(Map.Entry<String, String> key : headers.entrySet()) {
+                httpGet.addHeader(key.getKey(), key.getValue());
+            }
+            runningRequests.add(httpGet);
+
+            HttpResponse result = httpClient.execute(httpGet, localContext);
+            syncCookiesWithWebViews();
+            return result;
+        } catch (SSLException e) {
+            return null;
+        }
+    }
+
+    public boolean cloudFlareSolve(String responseString) {
+        // initialize Rhino
+        Context rhino = Context.enter();
+        try {
+            String domain = "www.diary.ru";
+            getPage(URI.create("http://" + domain), new HashMap<String, String>());
+            
+            // CF should wait
+            Thread.sleep(5000);
+            
+            // extract the arithmetic operation
+            Matcher operationSearch = OPERATION_PATTERN.matcher(responseString);
+            Matcher challengeSearch = CHALLENGE_PATTERN.matcher(responseString);
+            Matcher passSearch = PASS_PATTERN.matcher(responseString);
+            if(!operationSearch.find() || !passSearch.find() || !challengeSearch.find())
+                return false;
+            
+            String rawOperation = operationSearch.group(1);
+            String challengePass = passSearch.group(1);
+            String challenge = challengeSearch.group(1);
+            
+            
+            String operation = rawOperation
+                    .replaceAll("a\\.value =(.+?) \\+ .+?;", "$1")
+                    .replaceAll("\\s{3,}[a-z](?: = |\\.).+", "");
+            String js = operation.replace("\n", "");
+            
+            
+            rhino.setOptimizationLevel(-1);
+            Scriptable scope = rhino.initStandardObjects();
+            int result = ((Double) rhino.evaluateString(scope, js, "CloudFlare JS Challenge", 1, null)).intValue();
+            
+            String answer = String.valueOf(result + domain.length());
+
+            final List<NameValuePair> params = new ArrayList<>(3);
+            params.add(new BasicNameValuePair("jschl_vc", challenge));
+            params.add(new BasicNameValuePair("pass", challengePass));
+            params.add(new BasicNameValuePair("jschl_answer", answer));
+            
+            HashMap<String, String> headers = new HashMap<>(1);
+            headers.put("Referer", "http://www.diary.ru/");
+            
+            String url = "http://www.diary.ru/cdn-cgi/l/chk_jschl?" + URLEncodedUtils.format(params, "windows-1251");
+            
+            HttpResponse response = getPage(URI.create(url), headers);
+            if(response.getStatusLine().getStatusCode() == 200) {
+                response.getEntity().consumeContent();
+                return true;
+            }
+        } catch (Exception e) {
+            return false;
+        } finally {
+            Context.exit();
+        }
+        return false;
     }
 
     public interface ProgressListener {
