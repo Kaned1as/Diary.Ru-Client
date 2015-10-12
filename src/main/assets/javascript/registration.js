44531cfcb2353343b0ebcc24bb4aa40eea01a0d7
Адонай Элохим <adonai@xaker.ru>
2015-10-12 09:03:53 +0300

    removed excessive logging of JS part

--- src/main/assets/javascript/registration.js
+++ src/main/assets/javascript/registration.js
@@ -1,19 +1,13 @@
 window.addEventListener('load', delayedExecute, false);
 
 function delayedExecute() {
-    console.error("here");
     var token = $("input[name='adverigo_sid']")[0];
     var input = $("input[name='adverigo_captcha_answer']")[0];
     var send = $("input.submit")[0];
     if(!!token && !!input && !!send) {
-        console.error("here1");
         send.type = 'button'
         send.onclick = passToHandler;
     } else {
-        console.error("here2");
-        console.error(token);
-        console.error(input);
-        console.error(send);
         window.setTimeout(delayedExecute, 100);
     }
 }
@@ -21,6 +15,5 @@ function delayedExecute() {
 function passToHandler() {
     var token = $("input[name='adverigo_sid']")[0];
     var input = $("input[name='adverigo_captcha_answer']")[0];
-    console.error(RegisterHandler);
     RegisterHandler.grab(token.value, input.value);
 }
